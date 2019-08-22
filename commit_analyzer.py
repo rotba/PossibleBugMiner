225b4719521a24f7baa4566167fdd3a8b5adfa5c
אמיר אלמשעלי <amirelm@auth.ad.bgu.ac.il>
2019-08-22 11:54:45 +0300

    new criteria - changed components

--- commit_analyzer.py
+++ commit_analyzer.py
@@ -1,54 +1,88 @@
 import logging
+
+from aidnd_diff import commitsdiff
+from aidnd_diff.commit import Commit
+from mvnpy import mvn
 from pathlib import Path
 import os
 
 
 class IsBugCommitAnalyzer(object):
+    TESTS_DIFFS_IS_CRITERIA = True
 
-	def __init__(self, commit, repo):
-		self._commit = commit
-		self._repo = repo
-		self.associated_tests_paths = None
+    def __init__(self, commit, parent, repo):
+        self._commit = commit
+        self._parent = parent
+        self._repo = repo
+        self.associated_tests_paths = None
+        self.diffed_components = None
 
-	@property
-	def commit(self):
-		return self._commit
+    @property
+    def commit(self):
+        return self._commit
 
-	def analyze(self):
-		if len(self.commit.parents) == 0: return self
-		self.associated_tests_paths = self.get_tests_paths_from_commit()
-		return self
+    @property
+    def parent(self):
+        return self._parent
 
-	def is_bug_commit(self):
-		return self.associated_tests_paths != None and len(self.associated_tests_paths) > 0
+    def analyze(self):
+        if len(self.commit.parents) == 0: return self
+        self.associated_tests_paths = self.get_tests_paths_from_commit()
+        self.diffed_components = self.get_diffed_components()
+        return self
 
-	def get_test_paths(self):
-		return self.associated_tests_paths
+    def is_bug_commit(self):
+        return self.has_associated_tests_paths() and self.has_associated_diffed_components()
 
-	def get_tests_paths_from_commit(self):
-		ans = []
-		diff_index = self.commit.parents[0].diff(self.commit)
-		for file in self.commit.stats.files.keys():
-			if self.is_test_file(file):
-				try:
-					diff = list(filter(lambda d: d.a_path == file, diff_index))[0]
-				except IndexError as e:
-					logging.info('No diff for ' + file + ' in commit ' + self.commit.hexsha)
-					return ans
-				if not diff.deleted_file:
-					ans.append(os.path.join(self._repo.working_dir, file))
-		return ans
+    def get_test_paths(self):
+        return self.associated_tests_paths
 
-	def is_test_file(self, file):
-		name = os.path.basename(file.lower())
-		if not name.endswith('.java'):
-			return False
-		if name.endswith('test.java'):
-			return True
-		if name.endswith('tests.java'):
-			return True
-		if name.startswith('test'):
-			return True
-		if 'test' in os.path.basename(file).lower() and 'test' in Path(file).parts:
-			return True
-		return False
+    def get_tests_paths_from_commit(self):
+        ans = []
+        diff_index = self._parent.diff(self.commit)
+        for file in self.commit.stats.files.keys():
+            if self.is_test_file(file):
+                try:
+                    diff = list(filter(lambda d: d.a_path == file, diff_index))[0]
+                except IndexError as e:
+                    logging.info('No diff for ' + file + ' in commit ' + self.commit.hexsha)
+                    return ans
+                if not diff.deleted_file:
+                    ans.append(os.path.join(self._repo.working_dir, file))
+        return ans
+
+    def has_associated_tests_paths(self):
+        if not IsBugCommitAnalyzer.TESTS_DIFFS_IS_CRITERIA: return True
+        return self.associated_tests_paths is not None and len(self.associated_tests_paths) > 0
+
+    def has_associated_diffed_components(self):
+        return self.diffed_components is not None and len(self.diffed_components) > 0
+
+    def is_test_file(self, file):
+        name = os.path.basename(file.lower())
+        if not name.endswith('.java'):
+            return False
+        if name.endswith('test.java'):
+            return True
+        if name.endswith('tests.java'):
+            return True
+        if name.startswith('test'):
+            return True
+        if 'test' in os.path.basename(file).lower() and 'test' in Path(file).parts:
+            return True
+        return False
+
+
+    def get_diffed_components(self):
+        ans = []
+        commit_diff = commitsdiff.CommitsDiff(
+            commit_a=Commit.init_commit_by_git_commit(self.parent, 0),
+            commit_b=Commit.init_commit_by_git_commit(self.commit, 0))
+        for file_diff in commit_diff.diffs:
+            if file_diff.file_name.endswith('.java') and not self.is_test_file(file_diff.file_name):
+                file_path = os.path.join(self._repo.working_tree_dir, file_diff.file_name)
+                ans += list(
+                    map(lambda m: mvn.generate_mvn_class_names(src_path=file_path) + '#' + m,
+                        file_diff.changed_methods)
+                )
+        return ans
